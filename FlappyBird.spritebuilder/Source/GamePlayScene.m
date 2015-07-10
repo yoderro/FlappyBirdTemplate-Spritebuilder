ed718769ce4d29f8c39bc7167f6cea75d2079419
yoderro <robertyoder@Roberts-MacBook-Pro.local>
2015-07-09 23:29:10 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,16 @@
 - (void)initialize
 {
     // your code here
-}
-
+}character = (Character*)[CCBReader load:@"Character"];
+[physicsNode addChild:character];
 -(void)update:(CCTime)delta
 {
     // put update code here
 }
 
 // put new methods here
-
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
+    [character flap];
+}
+-(type I return)nameOfMethod: (type of first parameter) firstParameter continuation(
 @end
